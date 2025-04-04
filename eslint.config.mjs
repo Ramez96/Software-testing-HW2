c8e2cf5bb112ae5b06fa81ecf02731a3e9dcedf8
Ramez <ramri882@student.liu.se>
2025-04-04 13:47:27 +0800

    updated eslint config to newer standards

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,8 +2,10 @@ import { defineConfig } from "eslint/config";
 import globals from "globals";
 import js from "@eslint/js";
 
-
 export default defineConfig([
+  {
+    ignores: ["node_modules", "coverage", ".github"]
+  },
   { files: ["**/*.{js,mjs,cjs}"] },
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
   { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
